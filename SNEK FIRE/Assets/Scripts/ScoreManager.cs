c732be3132a4b4e12b1285e799e94f5de333a69e
Gabriel Venz Badilles <gvbadilles@up.edu.ph>
2024-05-30 09:50:25 +0800

    Updated ScoreManager to include high score

--- SNEK FIRE/Assets/Scripts/ScoreManager.cs
+++ SNEK FIRE/Assets/Scripts/ScoreManager.cs
@@ -5,18 +5,36 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    public int score;
+    public int currentScore;
     public TMP_Text scoreText;
+    public TMP_Text finalScoreText;
+    public TMP_Text highScoreText;
 
     // Start is called before the first frame update
     void Start()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Score: " + currentScore;
     }
 
     public void UpdateScore(int points)
     {
-        score += points;
-        scoreText.text = "Score: " + score;
+        currentScore += points;
+        scoreText.text = "Score: " + currentScore;
+    }
+
+    public void HighScoreUpdate()
+    {
+        if (PlayerPrefs.HasKey("SavedHighScore"))
+        {
+            if (currentScore > PlayerPrefs.GetInt("SavedHighScore"))
+            {
+                PlayerPrefs.SetIn("SavedHighScore", currentScore);
+            } else {
+                PlayerPrefs.SetIn("SavedHighScore", currentScore);
+            }
+
+            finalScoreText.text = currentScore.ToString();
+            highScoreText.text = PlayerPrefs.GetInt("SavedHighScore").ToString();
+        }
     }
 }
